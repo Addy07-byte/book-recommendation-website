fd1dfd104b841a74deb245221056bf504a8e436b
Aditya Hede <adithyahede7@gmail.com>
2025-07-31 15:11:36 -0700

    Update books.py
    Created list of my books

--- dsa/books.py
+++ dsa/books.py
@@ -1,1 +1,9 @@
-
+books = [
+  "The Monk Who Sold His Ferrari",
+  "rich-dad-poor-dad",
+  "ikigai",
+  "think-and-grow-rich",
+  "discover-your-destiny",
+  "think-like-a-monk",
+  "the-power-of-positive-thinking"
+]
